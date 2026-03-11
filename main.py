5aee68f347c9ea38d4ce2bda5c0ac5fbcbfbb846
stormdeveloper-glitch <stormdev.coder@gmail.com>
2026-03-11 13:39:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from urllib.request import urlopen
         
 load_dotenv()
 
-BOT_TOKEN= os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMINS = [6340253146]
 bot = telebot.TeleBot(BOT_TOKEN)
 TEACHER_PASSWORD = os.getenv("TEACHER_PASSWORD")
@@ -3872,4 +3872,5 @@ if __name__ == "__main__":
         bot.infinity_polling(timeout=10, long_polling_timeout=5)
     except Exception as e:
         print(f"❌ Botda kutilmagan xatolik: {e}")
+
 
